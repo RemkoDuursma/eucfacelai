17f343e95b0141febad900335b8262c9db24c23c
Remko Duursma <remkoduursma@gmail.com>
2015-05-11 13:39:52 +1000

    Uses Library, install packages only when not available

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+Library <- function(pkg, ...){
+  
+  PACK <- .packages(all.available=TRUE)
+  pkgc <- deparse(substitute(pkg))
+  
+  if(pkgc %in% PACK){
+    library(pkgc, character.only=TRUE)
+  } else {
+    install.packages(pkgc, ...)
+  }
+  
+}
